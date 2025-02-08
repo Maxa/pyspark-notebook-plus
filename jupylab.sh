97472fb00c1eb852769d17a6970cb460c61fb4c4
Maxa <53286886+Maxa@users.noreply.github.com>
2025-02-08 06:53:34 +0200

    replaced docker-compose with docker compose

--- jupylab.sh
+++ jupylab.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 SCRIPT_DIR=$(dirname $(realpath "$0"))
 up() {
-    docker-compose --project-directory "${SCRIPT_DIR}" up -d \
+    docker compose --project-directory "${SCRIPT_DIR}" up -d \
     && sleep 3 \
     && docker exec pyspark-jupylab sh -c "jupyter server list" \
     | sed -e 's,//[[:alnum:]]*:,//localhost:,'
 }
 
 down() {
-    docker-compose --project-directory "${SCRIPT_DIR}" down
+    docker compose --project-directory "${SCRIPT_DIR}" down
 }
 
 help() {
